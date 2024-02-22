de312c6b77b1c1281a75377d0c10318d8add1a2e
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-22 15:25:18 +0100

    exercise 1,2 solution added

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+/* esercizio 1 */
+
+/* let userInput = prompt("enter your email");
+
+const authorizedMail = ['fabioPacifici@gmail.com', 'ArturMamedov@gmail.com', 'GiulianoGostinfini@libero.it'];
+
+for(index =0; index < authorizedMail.length; index++){
+  if(userInput == authorizedMail[index]){
+    console.log("access in progress...");
+    break;
+  }
+  else{
+    console.log("incorrect mail, please start again");
+  }
+} */
+
+/* esercizio 2 */
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const myRoll = getRandomInt(6);
+console.log(myRoll);
+
+const cpuRoll = getRandomInt(6);
+console.log(cpuRoll);
+
+let myPoint = 0;
+let cpuPoint = 0;
+
+if(myRoll > cpuRoll){
+  console.log("you won!");
+  myPoint++;
+  
+}
+else if(myRoll < cpuRoll){
+  console.log("you lost!");
+  cpuPoint ++;
+}
+else{
+  console.log("draw, try again if you dare!");
+}
+
+console.log(myPoint);
+console.log(cpuPoint);
+
---
+++ assets/readme.md
@@ -0,0 +1,16 @@
+Descrizione
+
+1. Mail: chiedi all'utente la sua mail di accesso, controlla che questa sia nella lista delle mail autorizzate ad accedere, stampa un messaggio appropriato sull'esito del controllo.
+  human coding:
+  creare una array con le mail autorizzate
+  raccogliere il prompt dell'utente in una variabile;
+  comparare il valore con il nostro array
+  stampare il messaggio relativo all'operazione
+
+  strumenti:
+  - prompt
+  - array
+  - log
+  - for loop
+
+2. Gioco dei dadi: genera un numero random da 1 a 6, sia per il giocatore che per il computer. stabilire il vincitore in base a chi fa il punteggio più alto.
