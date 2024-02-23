a839d354a2b60c18aca3b2c8fb1ccc22663e7ec0
Carlo-Adile <adile.carlo.a@gmailcom>
2024-02-23 16:01:27 +0100

    fix index file position

--- assets/js/prove.js
+++ assets/js/prove.js
@@ -29,7 +29,7 @@ document.getElementById("my_form").addEventListener('submit', function (e) {
     let myAccess = document.createElement('h5');
     let myImage = document.createElement('img');
     myAccess.innerHTML = "Welcome back Fabio pacifici!";
-    myImage.src = "../assets/img/this_is_fabio.png";
+    myImage.src = "./assets/img/this_is_fabio.png";
 
     accessStatus.appendChild(myAccess);
     accessStatus.appendChild(myImage);
@@ -48,8 +48,8 @@ function rollTheDice() {
   let diceNum2 = document.getElementById("dice2");
 
   /* add gif animation */
-  diceNum1.setAttribute("src", "../assets/img/diceroll.gif")
-  diceNum2.setAttribute("src", "../assets/img/diceroll.gif")
+
+  diceNum2.setAttribute("src", "./assets/img/diceroll.gif")
 
   diceNum1.classList.add("dynamic-image");
   diceNum2.classList.add("dynamic-image");
@@ -64,8 +64,8 @@ function rollTheDice() {
     diceNum1.style.transform = "scale(1)";
     diceNum2.style.transform = "scale(1)";
 
-    diceNum1.setAttribute('src', '../assets/img/dice' + randomNumber1 + '.png');
-    diceNum2.setAttribute('src', '../assets/img/dice' + randomNumber2 + '.png');
+    diceNum1.setAttribute('src', './assets/img/dice' + randomNumber1 + '.png');
+    diceNum2.setAttribute('src', './assets/img/dice' + randomNumber2 + '.png');
 
     //determine the winner
     if (randomNumber1 === randomNumber2) {
